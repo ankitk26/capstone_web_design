1fac913b8855138827bf4f0bf8b84086b067a07a
Ankit Kumar <myselfankit51@gmail.com>
2020-07-01 03:27:13 +0530

    all files pushed

--- js/form.js
+++ js/form.js
@@ -6,23 +6,9 @@ function validate(){
 	var contact = document.getElementById("contact").value;
 	var dp = document.getElementById("dp").value;
 	var add = document.getElementById("add").value;
-	var error=0;
-	if (fname == "" || lname == "" || email == "" || dob == "" || contact == "" || dp == "" || add == ""){
-		alert("Please complete the form");
-		error=1;
-	}
-	if (contact.length < 10 && contact.length > 0){
-		alert("Please enter valid contact");
-		error=1;
-	}
-	if (email.indexOf("@") < 0){
-		alert("Please enter valid email");
-		error=1;
-	}
-	if (error=0){
-		alert("Thank you " + fname + " " + lname + " for joining our Library. Verification code is sent to your mail " + email);
-		return true;
+	if (fname == "" || lname == "" || email == "" || dob == "" || contact == "" || dp == "" || add == "" || email.indexOf("@") < 0 || contact.length<10){
+		alert("Invalid submission");
 	}
 	else
-		return false;
+		alert("Thank you " + fname + " " + lname + " for joining Central Park Library. Verification code has been sent to your email " + email);
 }
