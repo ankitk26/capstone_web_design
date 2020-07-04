20a280811a49376fac86286609721ee599c6676a
Ankit Kumar <myselfankit51@gmail.com>
2020-07-05 01:35:56 +0530

    web design for everybody capstone

--- js/form.js
+++ js/form.js
@@ -1,14 +1,22 @@
-function validate(){
-	var fname = document.getElementById("fname").value;
-	var lname = document.getElementById("lname").value;
-	var email = document.getElementById("email").value;
-	var dob = document.getElementById("dob").value;
-	var contact = document.getElementById("contact").value;
-	var dp = document.getElementById("dp").value;
-	var add = document.getElementById("add").value;
-	if (fname == "" || lname == "" || email == "" || dob == "" || contact == "" || dp == "" || add == "" || email.indexOf("@") < 0 || contact.length<10){
-		alert("Invalid submission");
+let form = document.getElementById("registerForm");
+form.addEventListener('submit', validate);
+
+function validate(e){
+	e.preventDefault();
+	
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
+	let email = document.getElementById("email").value;
+	let dob = document.getElementById("dob").value;
+	let contact = document.getElementById("contact").value;
+	let dp = document.getElementById("dp").value;
+	let add = document.getElementById("add").value;
+
+	if (fname == " " || lname == " " || email == " " || dob == " " || contact == " " || dp == " " || add == " " || email.indexOf('@')<0 || contact.length != 10){
+		alert('Invalid submission');
 	}
-	else
-		alert("Thank you " + fname + " " + lname + " for joining Central Park Library. Verification code has been sent to your email " + email);
+	else{
+		alert("Thank you " + fname + " " + lname + " for joining Central Park Library. Verification code has been sent to the email " + email);
+	}
+
 }
